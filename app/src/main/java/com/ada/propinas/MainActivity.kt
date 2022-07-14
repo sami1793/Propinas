033dff006383c21804c9ba6e98745637f1075869
sami1793 <sami_17_94@hotmail.com>
2022-07-14 13:05:13 -0300

    Se da la logica a boton en MainActivity.kt

--- app/src/main/java/com/ada/propinas/MainActivity.kt
+++ app/src/main/java/com/ada/propinas/MainActivity.kt
@@ -2,10 +2,38 @@ package com.ada.propinas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ada.propinas.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.botonCalcular.setOnClickListener { calcularPropina() }
+    }
+
+    fun calcularPropina() {
+        val stringEnCampoTexto = binding.costoDeServicio.text.toString()
+        val costo = stringEnCampoTexto.toDouble()
+        val selectedID = binding.opcionesPropina.checkedRadioButtonId
+
+        val porcentajePropina = when (selectedID) {
+            R.id.opcion_quince_porciento -> 0.15
+            R.id.opcion_dieciocho_porciento -> 0.18
+            else -> 0.20
+        }
+        var propina = costo * porcentajePropina
+
+        //PARTE DEL SWITCH
+        var redondear = binding.switchRedondeo.isChecked
+        if (redondear) {
+            propina = kotlin.math.ceil(propina)
+        }
+        val propinaFormateda = NumberFormat.getCurrencyInstance().format(propina)
+
+        binding.resultadoPropina.text = getString(R.string.monto_propina, propinaFormateda)
     }
 }
